3b94e5221b4418bcf81c75df7e9e03d12d376a51
Boyun <96613283+BoyunC@users.noreply.github.com>
2023-05-24 17:59:33 +0900

    의사 및 간호사 기능 추가 및 객체 생성자 작성
    의사
    - 의사 등록
    - 의사 검색
    간호사
    - 간호사 등록
    - 간호사 검색
    약, 바이탈  객체 생성자

--- src/people/Patient.java
+++ src/people/Patient.java
@@ -5,62 +5,66 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import medicine.Medicine;
 
-public class Patient extends Person{
+public class Patient extends Person {
 	private String regiNum;
-	private int visitCnt; 
+	private int visitCnt;
 	private Date firstVisitDate;
 	private Date lastVistiDate;
 	private String description;
 	private ArrayList<Vital> vital;
-	
+	private ArrayList<Medicine> takingMedicineList;
+	// private ArrayList<Payment>;
+
 	public static ArrayList<Patient> patientList = new ArrayList<Patient>();
-	
-	private Date date =  new Date();
-	
+
+	private Date date = new Date();
+
 	// Constructor
 	public Patient() {
-		visitCnt = 1;
-		firstVisitDate =  new Date();
-		lastVistiDate =  new Date();
-		vital = new ArrayList<Vital>();
-		
 	}
-	
-	// C : 이름, 코드, 주민등록번호
-	public Patient(String name, String code, String regiNum) {
+
+	// C : 이름, 코드, 주민등록번호, 특이사항, 복용약 리스트
+	public Patient(String name, String code, String regiNum, String description,
+			ArrayList<Medicine> takingMedicineList) {
 		super(name, code);
 		this.regiNum = regiNum;
-	
-		// 나이 설정 제대로 안됨 => 나이 계산기 
-		this.setAge(1900 + date.getYear() - (Integer.parseInt("19" + regiNum.charAt(0)+regiNum.charAt(1))));
-		
+
+		// 나이 설정 제대로 안됨 => 나이 계산기 이후 제작
+		this.setAge(1900 + date.getYear() - (Integer.parseInt("19" + regiNum.charAt(0) + regiNum.charAt(1))));
+
+		this.visitCnt = 1;
+		this.firstVisitDate = new Date();
+		this.lastVistiDate = new Date();
+		this.description = description;
+		this.takingMedicineList = takingMedicineList;
+		vital = new ArrayList<Vital>();
+
 		patientList.add(this);
 		printResult();
-		
-		visitCnt = 1;
-		firstVisitDate =  new Date();
-		lastVistiDate =  new Date();
-		vital = new ArrayList<Vital>();
 	}
 
-	// C : 이름, 코드, 비밀번호, 주민등록번호 
-	public Patient(String name, String code, String pw, String regiNum) {
+	// C : 이름, 코드, 비밀번호, 주민등록번호, 특이사항, 복용약 리스트
+	public Patient(String name, String code, String pw, String regiNum, String description,
+			ArrayList<Medicine> takingMedicineList) {
 		super(name, code, pw);
 		this.regiNum = regiNum;
-		
+
+		visitCnt = 1;
+		firstVisitDate = new Date();
+		lastVistiDate = new Date();
+		this.description = description;
+		this.takingMedicineList = takingMedicineList;
+		vital = new ArrayList<Vital>();
+
 		patientList.add(this);
 		printResult();
-		
-		visitCnt = 1;
-		firstVisitDate =  new Date();
-		lastVistiDate =  new Date();
-		vital = new ArrayList<Vital>();
 	}
 
-	// Method 
-	
-	// 
+	// Method
+
+	//
 	public void printPatient() {
 		System.out.println("성명 : " + this.getName());
 		System.out.println("나이 : " + this.getAge());
@@ -69,44 +73,35 @@ public class Patient extends Person{
 		System.out.println("첫 내원일 : " + this.getFirstVisitDate());
 		System.out.println("마지막 내원일 : " + this.getLastVistiDate());
 		System.out.println("특이사항 : " + this.getDescription());
-		
-		//System.out.println("환자 성명 : " + patient()); 복용약
+
+		// System.out.println("환자 성명 : " + patient()); 복용약
 	}
-	
-	// 회원 검색 
+
+	// 회원 검색
 	static public Patient getPatient(String name) {
-		for(Patient patient : patientList) {
+		for (Patient patient : patientList) {
 			if (patient.getName().equals(name)) {
 				return patient;
 			}
 		}
-		
-		return null;  // 존재하지 않는 경우 => null 반환 
+
+		return null; // 존재하지 않는 경우 => null 반환
 	}
-	
-	// 
+
+	//
 	private void printResult() {
 		System.out.println("회원 등록이 완료되었습니다.");
 	}
-	
+
 	//
 	public void printAll() {
-		System.out.println(this.getId() + " "
-				 + this.getPw() + " "
-				 + this.getName() + " "
-				 + this.getAge() + " "
-				 + this.getCode() + " "
-				 + this.regiNum);
-		
-	}
-	
-	
-	
-	
+		System.out.println(this.getId() + " " + this.getPw() + " " + this.getName() + " " + this.getAge() + " "
+				+ this.getCode() + " " + this.regiNum);
 
-	
-	// Getter & Setter 
-	
+	}
+
+	// Getter & Setter
+
 	public String getRegiNum() {
 		return regiNum;
 	}
@@ -170,5 +165,5 @@ public class Patient extends Person{
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
+
 }
