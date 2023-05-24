32ffed3dfb6550461f6a47a214032d3070880721
jslgi <zhem925@gmail.com>
2023-05-24 10:20:25 +0900

    create Paper object

---
+++ src/paper/Paper.java
@@ -0,0 +1,12 @@
+package paper;
+
+import java.util.Date;
+
+public class Paper {
+	private Date printDate;
+	private Date jinlyoDate; //Áø·á ¹¹¶ó±¸ ÇÒ°ÅÂ¡..
+	private int numOfPaper;
+	//private Hospital hospital;
+	//private Diagnosis diagnosis;
+	
+}
