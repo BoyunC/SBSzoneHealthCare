28590c7c3136fbf2c1cc8ca5ac7b7df0e7710590
Boyun <96613283+BoyunC@users.noreply.github.com>
2023-05-24 10:09:56 +0900

    create People object
    patient, doctor, nurse, vital

---
+++ src/people/Vital.java
@@ -0,0 +1,11 @@
+package people;
+
+import java.util.Date;
+
+public class Vital {
+	private double temperature;
+	private double weight;
+	private double height;
+	private String bloodPressure;
+	private Date date;
+}
