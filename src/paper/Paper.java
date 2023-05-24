52b9071d0f4cfd947e385dbf1e792a3edad27906
jslgi <zhem925@gmail.com>
2023-05-24 10:33:20 +0900

    update Paper object

--- src/paper/Paper.java
+++ src/paper/Paper.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Paper {
 	private Date printDate;
-	private Date jinlyoDate; //Áø·á ¹¹¶ó±¸ ÇÒ°ÅÂ¡..
+	private Date examDate; 
 	private int numOfPaper;
 	//private Hospital hospital;
 	//private Diagnosis diagnosis;
