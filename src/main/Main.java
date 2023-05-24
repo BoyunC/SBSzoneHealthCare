0fcb7010f32802bba3c92b3d963f06d6e44d57df
Boyun <96613283+BoyunC@users.noreply.github.com>
2023-05-24 15:07:02 +0900

    1. 환자 검색 기능 수정 및 메인 예시 작성

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,33 @@
 package main;
 
+import java.util.Scanner;
+
+import people.Patient;
+import people.PatientFunction;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		Scanner sc = new Scanner(System.in);
+		
+		// 임시 환자 데이터
+		new Patient("신짱구", "환자", "940819");
+		new Patient("신짱구", "환자", "930319");
+		new Patient("김철수", "환자", "921218");
+		new Patient("신짱구", "환자", "951201");
+		new Patient("신짱구", "환자", "960105");
+		new Patient("김철수", "환자", "971201");
+		new Patient("김철수", "환자", "981105");
 		
 		
+		
+		// 환자 검색 기능 사용 방법 
+		System.out.print("검색할 이름 입력 : ");
+		String name = sc.nextLine();
+		
+		Patient patient = PatientFunction.searchPatient(name);
+		
+		patient.printPatient();
 	}
 }
--- src/people/PatientFunction.java
+++ src/people/PatientFunction.java
@@ -1,5 +1,39 @@
 package people;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PatientFunction {
+	
+	Patient patient;
+
+	// 환자 검색 기능
+	public static Patient searchPatient(String name) {
+
+		ArrayList<Patient> searchPatientList = new ArrayList<Patient>();
+		int cnt = 1; 
+
+		// 동명이인 추출 
+		for (int i = 0; i < Patient.patientList.size(); i++) {
+			if (Patient.patientList.get(i).getName().equals(name)) {
+				searchPatientList.add(Patient.patientList.get(i));
+				System.out.println((cnt++) + ". " + Patient.patientList.get(i).getName() + "("
+						+ Patient.patientList.get(i).getRegiNum() + ")");
+			}
+		}
+
+		// 환자 존재하지 않는 경우 
+		if (searchPatientList.isEmpty()) {
+			System.out.println("해당하는 이름의 환자가 존재하지 않습니다.");
+			return null;
+		}
+
+		System.out.print("환자 선택 : ");
+		Scanner sc = new Scanner(System.in);
+		int pickPatient = Integer.parseInt(sc.nextLine()) - 1;
+		
+		return searchPatientList.get(pickPatient);
+	}
+
 
 }
