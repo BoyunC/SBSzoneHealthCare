ddc4d427d0eced347b352b3cf610be54d753a147
jslgi <zhem925@gmail.com>
2023-05-25 11:55:02 +0900

    커밋

--- src/paper/DischargeInstruction.java
+++ src/paper/DischargeInstruction.java
@@ -6,28 +6,23 @@ import people.Patient;
 public class DischargeInstruction extends Paper {
 	private Patient patient;
 	private Doctor doctor;
-	//private Exam exam;
 	
 	public void printDischargeInstruction() {
 		System.out.println("===================================");
-		System.out.println();
+		System.out.println("			       진단서	  		       ");
 		System.out.println();
 		System.out.printf("\t\t성명 : %s", patient.getName());
 		System.out.printf("\t\t주민등록번호: %s", patient.getRegiNum());
 		System.out.printf("\t\t진단명: %s");
 		System.out.printf("\t\t진단일: %s");
 		System.out.printf("\t\t진단의: %s", doctor.getName());
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
-		System.out.println("=                                 =");
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		System.out.println("===================================");
 	}
 	
+	public void fileDischargeInstruction() {
+		
+	}
 }
